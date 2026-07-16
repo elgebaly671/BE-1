0b775f432b25275ad5f89ae07e40dd09e1489e43
elgebaly671 <abdelrhman.elgebaly@student.guc.edu.eg>
2026-07-16 23:48:23 +0300

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const express = require('express');
 const app = express()
 const port = 3000;
 
+const tasks = [{
+    id: 1,
+    title: "Make the bed",
+    done: true
+}, {
+    id: 2,
+    title: "Wash the dishes",
+    done: false
+}, {
+    id: 3,
+    title: "feed the dog",
+    done: false
+}]
 app.get('/', (req, res) => {
     res.json({
         "name": "Task API",
@@ -12,10 +25,35 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/health', (req,res)=>{
+app.get('/health', (req, res) => {
     res.status(200).json({
         "status": "ok"
     })
+})
+
+app.get('/tasks', (req, res) => {
+    res.status(200).json({
+        tasks
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const { id } = req.params;
+
+    const task = tasks.find((item) => {
+
+        return item.id == Number(id)
+    })
+
+    if (!task)
+        return res.status(404).json({
+            error: `Task with id ${id} is not found`
+        })
+
+    return res.status(200).json({
+        task: task
+    })
+
 })
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
